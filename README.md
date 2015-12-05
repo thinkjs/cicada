e76992caef87d3537f934472829384d4f5e59bdf
akira-cn <akira.cn@gmail.com>
2015-12-06 00:02:03 +0800

    让国际化不区分大小写

--- README.md
+++ README.md
@@ -27,7 +27,8 @@ export default {
   port: 5678, //service start port
   protocol: 'http', //domain protocol
   token: 'TOKEN_VALUE', //token to check before every operation
-  website_title: 'welefen\'s favorites' //site's title
+  website_title: 'welefen\'s favorites', //site's title
+  visibility: 'public'  //set private to check token before view articles
 };
 ```
 
--- src/common/bootstrap/middleware.js
+++ src/common/bootstrap/middleware.js
@@ -11,3 +11,22 @@
  * })
  * 
  */
+
+'use strict';
+
+let locale = think.config('locale'), acceptLanguages, acceptLanguagesLowerCase;
+if(locale){ 
+  acceptLanguages = Object.keys(locale);
+  acceptLanguagesLowerCase = acceptLanguages.map((o) => o.toLowerCase());
+}
+
+think.middleware("check_lang", http => {  
+  if(acceptLanguages){
+    let lang = http.header('accept-language');
+    lang = lang.split(',')[0];
+    let idx = acceptLanguagesLowerCase.indexOf(lang);
+    if(idx >= 0){
+      http.lang(acceptLanguages[idx]);
+    } 
+  }
+});
