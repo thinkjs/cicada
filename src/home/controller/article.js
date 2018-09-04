777268426d95b589202247ebbc57cf3c836cf20e
lizheming <secretlzm007@gmail.com>
2018-09-04 22:11:33 +0800

    fix #27

--- src/home/controller/article.js
+++ src/home/controller/article.js
@@ -8,26 +8,26 @@ export default class extends Base {
    * index action
    * @return {Promise} []
    */
-  async listAction(){
+  async listAction() {
     let where = {};
-    let {keyword} = this.get();
+    let { keyword } = this.get();
 
-    if(keyword && keyword.toLowerCase().indexOf('tag:') === 0){
+    if (keyword && keyword.toLowerCase().indexOf('tag:') === 0) {
       //search by tag
       let tagdata = await this.model('tag').join({
         table: 'article_tag',
         join: 'inner',
         on: ['id', 'tag_id']
       })
-      .where({name: ['in', keyword.slice(4).split(',')]})
-      .page(this.get('page'), 10).select();
+        .where({ name: ['in', keyword.slice(4).split(',')] })
+        .page(this.get('page'), 10).select();
 
       where.id = ['IN', tagdata.map(d => d.article_id).join()];
 
       keyword = '';
     }
 
-    if(keyword){
+    if (keyword) {
       where = {
         title: ['LIKE', `%${keyword}%`],
         summary: ['LIKE', `%${keyword}%`],
@@ -62,22 +62,22 @@ export default class extends Base {
     this.assign('pagerData', data);
     this.assign('isLogin', this.cookie('token') === this.config('token'));
     this.assign('think', think);
-    this.assign('bookmarks', 'javascript:'+encodeURIComponent(bookmarksUrl));
-    this.assign('notemarks', 'javascript:'+encodeURIComponent(notesUrl));
+    this.assign('bookmarks', 'javascript:' + encodeURIComponent(bookmarksUrl));
+    this.assign('notemarks', 'javascript:' + encodeURIComponent(notesUrl));
 
     return this.display();
   }
   /**
    * add page
    */
-  addAction(){
-    let {url, tag} = this.get();
-    if(url.startsWith('/note/')){
+  addAction() {
+    let { url, tag } = this.get();
+    if (url.startsWith('/note/')) {
       this.assign('isNote', true);
     }
-    if(!tag && url.startsWith('/note/new')){
+    if (!tag && url.startsWith('/note/new')) {
       this.assign('tag', this.locale('note-tag'));
-    }else{
+    } else {
       this.assign('tag', tag);
     }
 
@@ -86,31 +86,31 @@ export default class extends Base {
   /**
    * edit page
    */
-  async editAction(){
-    let {id} = this.get();
+  async editAction() {
+    let { id } = this.get();
     let model = this.model('article');
-    let findData = await model.where({id: id}).find();
+    let findData = await model.where({ id: id }).find();
 
     delete findData.snapshot;
     delete findData.create_time;
 
     findData.tag = findData.tag.map(o => o.name).join();
 
-    let url = require('url').format({pathname: '/article/add', query: findData});
+    let url = require('url').format({ pathname: '/article/add', query: findData });
 
     return this.redirect(url);
   }
   /**
    * add action
    */
-  async saveAction(){
+  async saveAction() {
     let data = this.post();
 
     //article tags
     let tags = data.tag;
-    if(tags){
+    if (tags) {
       tags = tags.split(',').map(item => item.trim());
-    }else{
+    } else {
       tags = [];
     }
 
@@ -123,9 +123,9 @@ export default class extends Base {
     };
 
     let result = await model.thenAdd(record,
-      {url: data.url}).catch(() => false);
+      { url: data.url }).catch(() => false);
 
-    if(result === false){
+    if (result === false) {
       return this.fail('SAVE_FAIL');
     }
 
@@ -133,11 +133,11 @@ export default class extends Base {
 
     let isNote = data.url.startsWith('/note/');
 
-    if(isNote){
+    if (isNote) {
       record.url = '/note/' + record.id;
     }
 
-    if(result.type === 'exist' || isNote){
+    if (result.type === 'exist' || isNote) {
       await model.update(record);
     }
 
@@ -145,13 +145,13 @@ export default class extends Base {
     this.success();
   }
 
-  snapshot(article_id, url, rawContent){
+  snapshot(article_id, url, rawContent) {
     let service = this.service('spider');
     let spiderInstance = new service(url);
     let content_clean = spiderInstance.getCleanContent(rawContent);
-    return spiderInstance.run().then( content => {
-      if(content === 'error!') content = rawContent;
-      return this.model('snapshot').thenAdd({article_id, content, content_clean}, {article_id})
+    return spiderInstance.run().then(content => {
+      if (content === 'error!') content = rawContent;
+      return this.model('snapshot').thenAdd({ article_id, content, content_clean }, { article_id })
     });
   }
   /**
@@ -159,24 +159,27 @@ export default class extends Base {
    */
   async deleteAction() {
     let id = this.get('id');
-    let articleResult = await this.model('article').where({id}).delete();
-    let snapshotResult = await this.model('snapshot').where({article_id: id}).delete();
-
-    if( !articleResult || !snapshotResult) return this.fail('DELETE_FAIL');
-    this.redirect( this.referrer() || '/' );
+    try {
+      await this.model('article').where({ id }).delete();
+      await this.model('snapshot').where({ article_id: id }).delete();
+      this.redirect(this.referrer() || '/');
+    } catch (e) {
+      console.error(e);
+      return this.fail('DELETE_FAIL');
+    }
   }
   /**
    * snapshot
    * @return {} []
    */
-  async snapshotAction(){
+  async snapshotAction() {
     let id = this.get('id');
     let type = this.get('type');
     let field = type === 'clean' ? 'content_clean' : 'content';
-    let snapPromise = this.model('snapshot').where({article_id: id}).field(field).find();
-    let infoPromise = this.model('article').setRelation(false).where({id: id}).find();
+    let snapPromise = this.model('snapshot').where({ article_id: id }).field(field).find();
+    let infoPromise = this.model('article').setRelation(false).where({ id: id }).find();
     let [snap, info] = await Promise.all([snapPromise, infoPromise]);
-    if(think.isEmpty(snap)){
+    if (think.isEmpty(snap)) {
       return this.fail('ID_NOT_EXIST');
     }
     let content = snap[field];
@@ -189,8 +192,8 @@ export default class extends Base {
 
   async kindleAction() {
     let article_id = this.get('id');
-    let snap = await this.model('snapshot').where({article_id}).field('content').find();
-    if(think.isEmpty(snap)) {
+    let snap = await this.model('snapshot').where({ article_id }).field('content').find();
+    if (think.isEmpty(snap)) {
       return this.fail('ID_NOT_EXIST');
     }
     let service = this.service('kindle');
